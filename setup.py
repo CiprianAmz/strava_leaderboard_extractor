97bcf1a68667bd0c7b15ee822f84192c77df5a9e
ciprianamz <amzciprian@gmail.com>
2024-05-02 19:37:17 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="strava_leaderboard_extractor",
+    version="1.0",
+    description="Python utility to extract strava leaderboard",
+    packages=["strava_leaderboard_extractor"],
+)
